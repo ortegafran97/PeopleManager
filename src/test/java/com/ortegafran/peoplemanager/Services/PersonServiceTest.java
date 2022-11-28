1f3e338a917c0d3b51673b83c51f55e1848cca96
Franco Ortega <ortegafran97@gmail.com>
2022-11-28 11:39:44 -0300

    Tests fix (residency service add)

--- src/test/java/com/ortegafran/peoplemanager/Services/PersonServiceTest.java
+++ src/test/java/com/ortegafran/peoplemanager/Services/PersonServiceTest.java
@@ -1,17 +1,15 @@
 package com.ortegafran.peoplemanager.Services;
 
 import com.ortegafran.peoplemanager.Model.Entities.Person;
+import com.ortegafran.peoplemanager.Model.Entities.Residency;
 import com.ortegafran.peoplemanager.Model.Repositories.PersonRepository;
-import org.junit.jupiter.api.BeforeAll;
+import com.ortegafran.peoplemanager.Model.Repositories.ResidencyRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 
 import java.util.*;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,14 +17,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonServiceTest {
     PersonService personService;
     List<Person> personList = Arrays.asList(
-            new Person(UUID.fromString("b1b2045b-0a4c-40ee-b43c-b19cf8f05bc7"),"65465454","Franco","Ortega","varas"),
-            new Person("23435234","Angel","Dominguez","Perez"),
-            new Person("62362351","Rafael","Gomez","Gonzales"),
-            new Person("12312312","Francisco","Crespo","Otamendi"));
+            new Person(UUID.fromString("b1b2045b-0a4c-40ee-b43c-b19cf8f05bc7"),
+                    "65465454",
+                    "Franco",
+                    "Ortega",
+                    "varas",
+                    new Residency("S.M. del carril",
+                        "btst1",
+                        "btst2",
+                        300,
+                        "Norte",
+                        "comment")),
+            new Person("23435234","Angel","Dominguez","Perez",null),
+            new Person("62362351","Rafael","Gomez","Gonzales",null),
+            new Person("12312312","Francisco","Crespo","Otamendi",null));
 
     @BeforeEach
     public void setUp(){
         PersonRepository personRepository = Mockito.mock(PersonRepository.class);
+        ResidencyService residencyService = new ResidencyService(Mockito.mock(ResidencyRepository.class));
 
         Mockito.when(personRepository.findAll())
                 .thenReturn(personList);
@@ -34,12 +43,12 @@ class PersonServiceTest {
         Mockito.when(personRepository.save(Mockito.any(Person.class)))
                 .thenAnswer(invocation -> invocation.getArguments()[0]);
 
-        personService = new PersonService(personRepository);
+        personService = new PersonService(personRepository, residencyService);
     }
 
     @Test
     public void accept_person_with_new_dni() {
-        Person newPerson = new Person("123","Nueva","Persona","Test");
+        Person newPerson = new Person("123","Nueva","Persona","Test",null);
 
         Optional<Person> saved = personService.addOne(newPerson);
 
@@ -48,7 +57,7 @@ class PersonServiceTest {
 
     @Test
     public void reject_repeated_dni(){
-        Person newPerson = new Person("65465454","Repeated","Persona","Test");
+        Person newPerson = new Person("65465454","Repeated","Persona","Test",null);
         Optional<Person> saved = personService.addOne(newPerson);
         assertEquals(Optional.empty(),saved);
     }
@@ -58,15 +67,13 @@ class PersonServiceTest {
         assertTrue(personService.findAll().stream().count()>0);
     }
 
-//    @Test
+    @Test
     public void find_person_by_id(){
-//        Optional<Person> found = personService.findById(UUID.fromString("b1b2045b-0a4c-40ee-b43c-b19cf8f05bc7"));
-        Optional<Person> found = personService.findByDni("65465454");
+        Person expected = personList.get(0);
 
-        System.out.println(found);
-
+        Optional<Person> found = personService.findById(expected.getId());
         assertEquals(
-                new Person(UUID.fromString("b1b2045b-0a4c-40ee-b43c-b19cf8f05bc7"),"65465454","Franco","Ortega","varas"),
+                expected,
                 found.get()
         );
 

d2430554fa1feb6e0ee051a795b516b1860fc76a
Franco Ortega <ortegafran97@gmail.com>
2022-12-02 13:39:01 -0300

    New insertions, jobs, Residency

--- src/main/java/com/ortegafran/peoplemanager/runner/TestRunner.java
+++ src/main/java/com/ortegafran/peoplemanager/runner/TestRunner.java
@@ -2,33 +2,43 @@ package com.ortegafran.peoplemanager.runner;
 
 import com.ortegafran.peoplemanager.Model.Entities.Person;
 import com.ortegafran.peoplemanager.Model.Entities.Residency;
+import com.ortegafran.peoplemanager.Model.Repositories.JobRecordRepository;
 import com.ortegafran.peoplemanager.Model.Repositories.PersonRepository;
 import com.ortegafran.peoplemanager.Model.Repositories.ResidencyRepository;
+import com.ortegafran.peoplemanager.Services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Component
 public class TestRunner implements CommandLineRunner{
     @Autowired
     private final PersonRepository personRepository;
-
+    @Autowired
+    private final PersonService personService;
     @Autowired
     private final ResidencyRepository residencyRepository;
+    @Autowired
+    private final JobRecordRepository jobRecordRepository;
 
-    public TestRunner(PersonRepository personRepository, ResidencyRepository residencyRepository) {
+    public TestRunner(PersonRepository personRepository, PersonService personService, ResidencyRepository residencyRepository, JobRecordRepository jobRecordRepository) {
         this.personRepository = personRepository;
+        this.personService = personService;
         this.residencyRepository = residencyRepository;
+        this.jobRecordRepository = jobRecordRepository;
     }
 
     @Override
-    public void run(String... args) throws Exception{
-        personRepository.save(new Person(
+    public void run(String... args) throws Exception {
+        actions();
+    }
+
+    private void actions(){
+        Person p1 = new Person(
                 UUID.fromString("b1b2045b-0a4c-40ee-b43c-b19cf8f05bc7"),
-                "65465454","Franco",
+                "65465454", "Franco",
                 "Ortega",
                 "varas",
                 new Residency("S.M. del carril",
@@ -36,24 +46,29 @@ public class TestRunner implements CommandLineRunner{
                         "btst2",
                         300,
                         "Norte",
-                        "comment")));
-        personRepository.save(new Person("23435234","Angel","Dominguez","Perez",null));
-        personRepository.save(new Person("62362351","Rafael","Gomez","Gonzales",null));
-        personRepository.save(new Person("12312312","Francisco","Crespo","Otamendi",null));
+                        "comment")
+        );
+        Person p2 = new Person("23435234", "Angel", "Dominguez", "Perez", null);
 
-        Optional<Person> person = personRepository.findByDni("65465454");
+        personService.addOne(p1);
+        personService.addOne(p2);
+        personService.addOne(new Person("62362351", "Rafael", "Gomez", "Gonzales", null));
+        personService.addOne(new Person("12312312", "Francisco", "Crespo", "Otamendi", null));
 
-        if(person.isPresent()){
-            Person p = person.get();
-            Residency r = new Residency("S.M. del carril",
-                    "btst1",
-                    "btst2",
-                    300,
-                    "Norte",
-                    "comment");
+        /* Jobs insertions */
+        /* JobRecord j1 = new JobRecord("GL",
+                "GL",
+                LocalDateTime.of(2020,01,01,00,00),
+                null);
+        j1.setPerson(p1);
 
-            p.setResidency(r);
-            personRepository.save(p);
-        }
+        jobRecordRepository.save(j1);
+
+        jobRecordRepository.findAll()
+                .stream().collect(Collectors.toList())
+                .forEach( jobRecord -> System.out.println(jobRecord));
+        */
     }
+
+
 }
